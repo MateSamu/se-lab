ba89b0e7159e5901bd5f497fcc5badbc9641969a
Mate Samuel <matesamuel1@gmail.com>
2023-10-04 16:39:04 +0200

    Sietek

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,8 +77,13 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
-        //TODO implement feature
+        if(primaryTorpedoStore.fire(1)){
+          firingSuccess = true;
+        } else if(secondaryTorpedoStore.fire(1)){
+          firingSuccess = true;
+        } else{
+          firingSuccess = false;
+        }
 
         break;
     }
